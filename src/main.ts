bde7a511df8ca0830bcbaff0682a522773a1a5a8
zhuqinfei <1586859762@qq.com>
2023-07-25 23:22:25 +0800

    配置element-plus和测试

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-expect-error  防止打包时候出错
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
 
+const app=createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
