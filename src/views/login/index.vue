426ec72d973de36c47728f17e82488b50b5afe1b
zhuqinfei <1586859762@qq.com>
2023-08-03 16:50:20 +0800

    自定义校验表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,7 +5,12 @@
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
         <!--登录表单-->
-        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到橙子运营平台</h2>
           <el-form-item prop="username">
@@ -54,7 +59,7 @@ let $router = useRouter()
 //路由对象
 let $route = useRoute()
 //获取el-form组件
-let loginForms = ref();
+let loginForms = ref()
 
 //收集账号与密码数据
 let loginForm = reactive({ username: 'admin', password: '111111' })
@@ -64,7 +69,7 @@ let loading = ref(false)
 //登录按钮的回调
 const login = async () => {
   // 保证全部表单相校验通过再发请求
-  await loginForms.value.validate();
+  await loginForms.value.validate()
   //按钮加载效果
   loading.value = true
   //点击登录按钮以后干什么
@@ -94,6 +99,27 @@ const login = async () => {
     })
   }
 }
+//自定义校验规则函数
+const validatorUserName = (rule: any, value: any, callback: any) => {
+  console.log(rule)
+  console.log(value)
+  //rule:即为校验规则对象
+  //value:即为表单元素文本内容
+  //函数:如果符合条件callBack放行通过即为
+  //如果不符合条件callBack方法,注入错误提示信息
+  if (value.length >= 5) {
+    callback();
+  } else {
+    callback(new Error('账号长度至少五位'));
+  }
+}
+const validatorPassword = (rule: any, value: any, callback: any) => {
+  if (value.length >= 6) {
+    callback();
+  } else {
+    callback(new Error('密码长度至少六位'));
+  }
+}
 
 //定义表单校验需要配置对象
 const rules = {
@@ -104,14 +130,14 @@ const rules = {
   //message:错误的提示信息
   //trigger:触发校验表单的时机 change->文本发生变化触发校验,blur:失去焦点的时候触发校验规则
   username: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-    { required: true, min: 6, max: 10, message: '账号长度至少六位', trigger: 'change' }
-    // { trigger: 'change', validator: validatorUserName }
+    // { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    // { required: true, min: 6, max: 10, message: '账号长度至少六位', trigger: 'change',},
+    { trigger: 'change', validator: validatorUserName }
   ],
   password: [
-    { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' }
-    // { trigger: 'change', validator: validatorPassword }
-  ]
+    // { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change',},
+    { trigger: 'change', validator: validatorPassword }
+  ],
 }
 </script>
 
