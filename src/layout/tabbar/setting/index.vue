a2cccd158055b7da778f4451da3c8285a698cbaa
zhuqinfei <1586859762@qq.com>
2023-08-11 02:24:22 +0800

    全屏模式的切换

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,7 +1,12 @@
 <!--suppress ALL -->
 <template>
-  <el-button size="small" icon="Refresh" circle @click="updateRefsh"></el-button>
-  <el-button size="small" icon="FullScreen" circle></el-button>
+  <el-button
+    size="small"
+    icon="Refresh"
+    circle
+    @click="updateRefsh"
+  ></el-button>
+  <el-button size="small" icon="FullScreen" circle @click="fullScreen"></el-button>
   <el-button size="small" icon="Setting" circle></el-button>
   <img
     src="../../../../../public/logo.png"
@@ -25,12 +30,27 @@
 
 <script setup lang="ts">
 //获取骨架的小仓库
-import useLayOutSettingStore from '@/store/modules/setting';
-let layoutSettingStore = useLayOutSettingStore();
+import useLayOutSettingStore from '@/store/modules/setting'
+let layoutSettingStore = useLayOutSettingStore()
+
 //刷新按钮点击回调
 const updateRefsh = () => {
-  layoutSettingStore.refsh = !layoutSettingStore.refsh;
-};
+  layoutSettingStore.refsh = !layoutSettingStore.refsh
+}
+
+//全屏按钮点击的回调
+const fullScreen = () => {
+  //DOM对象的一个属性:可以用来判断当前是不是全屏模式[全屏:true,不是全屏:false]
+  let full = document.fullscreenElement;
+  //切换为全屏模式
+  if (!full) {
+    //文档根节点的方法requestFullscreen,实现全屏模式
+    document.documentElement.requestFullscreen();
+  } else {
+    //变为不是全屏模式->退出全屏模式
+    document.exitFullscreen();
+  }
+}
 </script>
 
 <script lang="ts">
@@ -38,7 +58,5 @@ export default {
   name: 'Setting',
 }
 </script>
-
-
 
 <style scoped lang="scss"></style>
