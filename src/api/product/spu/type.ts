83b9f4fefd316ccb61f526271491c256c88725a2
zhuqinfei <1586859762@qq.com>
2023-08-30 15:59:01 +0800

    spu模块展示已有的spu数据

---
+++ src/api/product/spu/type.ts
@@ -0,0 +1,31 @@
+//服务器全部接口返回的数据类型
+export interface ResponseData {
+  code: number
+  message: string
+  ok: boolean
+}
+
+
+//SPU数据的ts类型:需要修改
+export interface SpuData {
+  category3Id: string | number
+  id?: number
+  spuName: string
+  tmId: number | string
+  description: string
+  spuImageList: null
+  spuSaleAttrList: null
+}
+//数组:元素都是已有SPU数据类型
+export type Records = SpuData[]
+//定义获取已有的SPU接口返回的数据ts类型
+export interface HasSpuResponseData extends ResponseData {
+  data: {
+    records: Records
+    total: number
+    size: number
+    current: number
+    searchCount: boolean
+    pages: number
+  }
+}
--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -4,39 +4,114 @@
     <!-- 三级分类 -->
     <Category :scene="scene"></Category>
     <el-card style="margin: 10px 10px">
-      <el-button type="primary" size="default" icon="Plus">添加SPU</el-button>
-      <el-table border style="margin: 10px 10px">
+      <el-button type="primary" size="default" icon="Plus" :disabled="categoryStore.c3Id ? false : true">添加SPU</el-button>
+      <el-table border style="margin: 10px 10px" :data="records">
         <el-table-column
-            label="序号"
-            type="index"
-            align="center"
-            width="80px"
+          label="序号"
+          type="index"
+          align="center"
+          width="80px"
         ></el-table-column>
-        <el-table-column label="SPU名称"></el-table-column>
-        <el-table-column label="SPU描述"></el-table-column>
-        <el-table-column label="SPU操作"></el-table-column>
+        <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+        <el-table-column label="SPU描述" prop="description"></el-table-column>
+        <el-table-column label="SPU操作">
+          <!-- row:即为已有的SPU对象 -->
+          <template #="{ row, $index }">
+            <el-button
+                type="primary"
+                size="small"
+                icon="Plus"
+                title="添加SKU"
+            ></el-button>
+            <el-button
+                type="primary"
+                size="small"
+                icon="Edit"
+                title="修改SPU"
+            ></el-button>
+            <el-button
+                type="primary"
+                size="small"
+                icon="View"
+                title="查看SKU列表"
+            ></el-button>
+            <el-popconfirm :title="`你确定删除${row.spuName}?`" width="200px">
+              <template #reference>
+                <el-button
+                    type="primary"
+                    size="small"
+                    icon="Delete"
+                    title="删除SPU"
+                ></el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
     <!-- 分页器 -->
     <el-pagination
-        v-model:current-page="pageNo"
-        v-model:page-size="pageSize"
-        :page-sizes="[3, 5, 7, 9]"
-        :background="true"
-        layout=" prev, pager, next, jumper,->, sizes,total"
-        :total="400"
+      v-model:current-page="pageNo"
+      v-model:page-size="pageSize"
+      :page-sizes="[3, 5, 7, 9]"
+      :background="true"
+      layout=" prev, pager, next, jumper,->, sizes,total"
+      :total="total"
+      @current-change="getHasSpu"
+      @size-change="changeSize"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onBeforeUnmount } from 'vue'
+import { ref, watch} from 'vue'
+import type { HasSpuResponseData,Records} from '@/api/product/spu/type'
+import { reqHasSpu} from '@/api/product/spu';
+//引入分类的仓库
+import useCategoryStore from '@/store/modules/category';
+let categoryStore=useCategoryStore()
 //场景的数据
 let scene = ref<number>(0)
 //分页器默认页码
 let pageNo = ref<number>(1)
 //每一页展示几条数据
 let pageSize = ref<number>(3)
+//存储已有的SPU的数据
+let records = ref<Records>([]);
+//存储已有SPU总个数
+let total = ref<number>(0);
+
+//监听三级分类ID变化
+watch(
+    () => categoryStore.c3Id,
+    () => {
+      //当三级分类发生变化的时候清空对应的数据
+      records.value = []
+      //务必保证有三级分类ID
+      if (!categoryStore.c3Id) return
+      getHasSpu()
+    },
+)
+
+//此方法执行:可以获取某一个三级分类下全部的已有的SPU
+const getHasSpu = async (pager = 1) => {
+  //修改当前页码
+  pageNo.value = pager
+  let result: HasSpuResponseData = await reqHasSpu(
+      pageNo.value,
+      pageSize.value,
+      categoryStore.c3Id,
+  )
+  if (result.code == 200) {
+    records.value = result.data.records
+    total.value = result.data.total
+  }
+}
+//分页器下拉菜单发生变化的时候触发
+const changeSize = () => {
+  getHasSpu()
+}
+
 </script>
 
 <style scoped lang="scss"></style>
