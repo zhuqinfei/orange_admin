6a087b01e33de170c0c357a678486ca7a5de4712
zhuqinfei <1586859762@qq.com>
2023-09-06 09:05:13 +0800

    sku的上架与下架

--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -17,15 +17,27 @@
       ></el-table-column>
       <el-table-column label="图片" width="300px">
         <template #="{ row, $index }">
-          <img :src="row.skuDefaultImg" alt="" style="width: 100px;height: 100px;">
+          <img
+            :src="row.skuDefaultImg"
+            alt=""
+            style="width: 100px; height: 100px"
+          />
         </template>
       </el-table-column>
-      <el-table-column label="重量" width="300px" prop="weight"></el-table-column>
-      <el-table-column label="价格" width="300px" prop="price"></el-table-column>
+      <el-table-column
+        label="重量"
+        width="300px"
+        prop="weight"
+      ></el-table-column>
+      <el-table-column
+        label="价格"
+        width="300px"
+        prop="price"
+      ></el-table-column>
       <el-table-column label="操作" width="300px" fixed="right">
         <template #="{ row, $index }">
-          <el-button type="primary" size="small" icon="Top"></el-button>
-          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" :icon="row.isSale == 1 ? 'Bottom' : 'Top'"  @click="updateSale(row)"></el-button>
+          <el-button type="primary" size="small" icon="Edit" @click="updateSku"></el-button>
           <el-button type="primary" size="small" icon="InfoFilled"></el-button>
           <el-button type="primary" size="small" icon="Delete"></el-button>
         </template>
@@ -45,14 +57,12 @@
 </template>
 
 <script setup lang="ts">
-import { ref,onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 //引入请求
-import { reqSkuList } from '@/api/product/sku'
+import { reqSkuList,reqCancelSale,reqSaleSku } from '@/api/product/sku'
 //引入ts类型
-import type {
-  SkuResponseData,
-  SkuData
-} from '@/api/product/sku/type'
+import type { SkuResponseData, SkuData } from '@/api/product/sku/type'
+import { ElMessage } from 'element-plus';
 //分页器当前页码
 let pageNo = ref<number>(1)
 //每一页展示几条数据
@@ -76,7 +86,33 @@ const getHasSku = async (pager = 1) => {
 
 //分页器下拉菜单发生变化触发
 const handler = (pageSizes: number) => {
-  getHasSku();
+  getHasSku()
+}
+
+//商品的上架与下架的操作
+const updateSale = async (row: SkuData) => {
+  //如果当前商品的isSale==1,说明当前商品是上架的额状态->更新为下架
+  //否则else情况与上面情况相反
+  if (row.isSale == 1) {
+    //下架操作
+    await reqCancelSale(row.id as number)
+    //提示信息
+    ElMessage({ type: 'success', message: '下架成功' })
+    //发请求获取当前更新完毕的全部已有的SKU
+    getHasSku(pageNo.value)
+  } else {
+    //下架操作
+    await reqSaleSku(row.id as number)
+    //提示信息
+    ElMessage({ type: 'success', message: '上架成功' })
+    //发请求获取当前更新完毕的全部已有的SKU
+    getHasSku(pageNo.value)
+  }
+}
+
+//更新已有的SKU
+const updateSku = () => {
+  ElMessage({ type: 'success', message: '程序员在努力的更新中....' })
 }
 
 </script>
