025f8b5d1ef2cf76e406968a5bd90c9dbbdc6c42
zhuqinfei <1586859762@qq.com>
2023-09-09 16:02:01 +0800

    数据大屏中国地图与飞机航线的制作

---
+++ src/views/screen/components/line/index.vue
@@ -0,0 +1,12 @@
+<!--suppress ALL -->
+<template>
+<div>11122</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -10,7 +10,10 @@
           <Sex class="sex"></Sex>
           <Age class="age"></Age>
         </div>
-        <div class="center">中间</div>
+        <div class="center">
+          <Map class="map"></Map>
+          <Line class="line"></Line>
+        </div>
         <div class="right">右侧</div>
       </div>
     </div>
@@ -25,6 +28,10 @@ import Top from './components/top/index.vue'
 import Tourist from './components/tourist/index.vue'
 import Sex from './components/sex/index.vue'
 import Age from './components/age/index.vue'
+
+//引入中间两个子组件
+import Map from './components/map/index.vue';
+import Line from './components/line/index.vue';
 
 //获取数据大屏展示内容盒子的DOM元素
 let screen = ref()
@@ -89,9 +96,16 @@ window.onresize = () => {
         }
       }
       .center {
-        flex: 1.5;
+        flex: 2;
         display: flex;
         flex-direction: column;
+        .map {
+          flex: 4;
+        }
+
+        .line {
+          flex: 1;
+        }
       }
     }
   }
